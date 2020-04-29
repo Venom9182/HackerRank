2154268e24ecc0db8a80f4f40e8bf105e2064cd9
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-04-29 19:28:28 +0530

    Update insert-a-node-at-the-tail-of-a-linked-list.py
    Node class can not be renamed as it is locked code. Although logic was right, but won't work in hackerrank

--- insert-a-node-at-the-tail-of-a-linked-list.py
+++ insert-a-node-at-the-tail-of-a-linked-list.py
@@ -1,8 +1,8 @@
-def Insert(head, data):
+def insertNodeAtTail(head, data):
     if head is None:
-        return Node(data)
+        return SinglyLinkedListNode(data)
     x = head
     while x.next is not None:
         x = x.next
-    x.next = Node(data)
+    x.next = SinglyLinkedListNode(data)
     return head
