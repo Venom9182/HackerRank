881c48a3966f724db27f0d4f4b54ee07c9923780
Kunal Sharma <85027477+Venom9182@users.noreply.github.com>
2023-08-10 16:37:56 +0530

    Jsxnew.jsx

--- Jsxnew.jsx
+++ Jsxnew.jsx
@@ -5,8 +5,8 @@ import logoImg from './logo f 1.png';
 const Login = (props) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [isUpperCase, setIsUpperCase] = useState(false);
-  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+  const [showVirtualKeyboard, setShowVirtualKeyboard] = useState(false);
+  
 
   const handleKeyPress = (key) => {
     if (key === 'clear') {
@@ -15,18 +15,9 @@ const Login = (props) => {
     } else if (key === 'backspace') {
       setUsername(username.slice(0, -1));
       setPassword(password.slice(0, -1));
-    } else if (key === 'upperCase') {
-      setIsUpperCase(!isUpperCase);
-    } else if (key === 'space') {
-      setUsername(username + ' ');
-      setPassword(password + ' ');
     } else {
-      const typedKey = isUpperCase ? key.toUpperCase() : key;
-      if (document.activeElement.id === 'username') {
-        setUsername(username + typedKey);
-      } else {
-        setPassword(password + typedKey);
-      }
+      setUsername(username + key);
+      setPassword(password + key);
     }
   };
 
@@ -34,7 +25,7 @@ const Login = (props) => {
     ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'backspace'],
-    ['upperCase', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'clear', 'space'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm', 'clear'],
   ];
 
   const handleSubmit = (e) => {
@@ -60,6 +51,7 @@ const Login = (props) => {
           <div className="col-md-6">
             <div className="login-container" style={{ backgroundColor: '#f2f2f2', float: 'right', float: 'top' }}>
               <h2 className="mb-4">Login</h2>
+              
               <form action="#" method="post" style={{ textAlign: 'center' }}>
                 <div className="form-group">
                   <label htmlFor="username">
@@ -67,16 +59,13 @@ const Login = (props) => {
                     Username:
                   </label>
                   <input
-                    type="number"
+                    type="text"
                     id="username"
                     name="username"
                     placeholder="Enter Username"
                     className="form-control"
-                    min="0"
-                    max="999999999"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
-                    onInput={(e) => (e.target.value = e.target.value.replace(/[^0-9]/g, ''))}
                     required
                   />
                 </div>
@@ -98,29 +87,43 @@ const Login = (props) => {
                     />
                   </div>
                 </div>
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  style={{ width: '200px', height: '40px', backgroundColor: '#007bff', borderRadius: '7px' }}
-                  onClick={() => setIsKeyboardOpen(!isKeyboardOpen)}
-                >
-                  Open Virtual Keyboard
+                <button type="submit" className="btn btn-primary" style={{ width: '150px', height: '40px', backgroundColor: '#007bff', borderRadius: '7px' }}>
+                  Login
                 </button>
-                <div className="forgot-password mt-4" style={{ marginTop: '10px' }}>
-                  <a href="#">Forgot Password?</a>
-                </div>
+                <button
+                  className="btn btn-secondary"
+                  style={{ marginLeft: '10px', borderRadius: '7px' }}
+                  onClick={() => setShowVirtualKeyboard(!showVirtualKeyboard)}
+                >
+                  Virtual Keyboard
+                </button>
               </form>
-              {isKeyboardOpen && (
-                <div className="virtual-keyboard">
-                  {/* Virtual keyboard buttons */}
-                </div>
-              )}
+              <div className="forgot-password mt-4" style={{ marginTop: '10px' }}>
+                <a href="#">Forgot Password?</a>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      {showVirtualKeyboard && (
+        <div className="virtual-keyboard">
+          {keyboardLayout.map((row, rowIndex) => (
+            <div key={rowIndex} className="keyboard-row">
+              {row.map((key, keyIndex) => (
+                <button
+                  key={keyIndex}
+                  onClick={() => handleKeyPress(key)}
+                  className="keyboard-key"
+                >
+                  {key}
+                </button>
+              ))}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-};
+}
 
-export default
+export default Login;
