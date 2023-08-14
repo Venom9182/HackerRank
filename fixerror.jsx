80010d6ac5b7934e9c3ad930b0157a42d8d17fca
Kunal Sharma <85027477+Venom9182@users.noreply.github.com>
2023-08-14 18:05:41 +0530

    Create fixerror.jsx

---
+++ fixerror.jsx
@@ -0,0 +1,10 @@
+export default function RecentlyViewed(props) {
+  const docs = [
+    {
+      URI: process.env.PUBLIC_URL + '/sample.pdf'
+    }
+    // Add more items to the array as needed
+  ];
+
+  // Rest of your component code
+}
